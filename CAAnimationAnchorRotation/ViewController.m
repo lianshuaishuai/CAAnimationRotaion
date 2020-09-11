ab703f55edece52e3f88e3caa5ecaaf757591221
连帅帅 <741312975@qq.com>
2020-09-11 15:15:06 +0800

    --增加恢复按钮

--- CAAnimationAnchorRotation/ViewController.m
+++ CAAnimationAnchorRotation/ViewController.m
@@ -31,14 +31,30 @@ static char shape_layer;
     rotaionBtn.backgroundColor = [UIColor blueColor];
     rotaionBtn.layer.cornerRadius = 5;
     [rotaionBtn setTitle:@"给我掉落" forState:(UIControlStateNormal)];
-    [rotaionBtn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
+    [rotaionBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
     [rotaionBtn addTarget:self action:@selector(fall:) forControlEvents:(UIControlEventTouchUpInside)];
+    
+    
+    UIButton *recoveryBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    [self.view addSubview:recoveryBtn];
+    objc_setAssociatedObject(recoveryBtn, &shape_layer, shapeLayer, OBJC_ASSOCIATION_RETAIN);
+    recoveryBtn.frame = CGRectMake(50, 300, 100, 30);
+    recoveryBtn.backgroundColor = [UIColor redColor];
+    recoveryBtn.layer.cornerRadius = 5;
+    [recoveryBtn setTitle:@"恢复" forState:(UIControlStateNormal)];
+    [recoveryBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
+    [recoveryBtn addTarget:self action:@selector(recovery:) forControlEvents:(UIControlEventTouchUpInside)];
     
 }
 
 -(void)fall:(UIButton *)sender{
     CAShapeLayer *shapeLayer = objc_getAssociatedObject(sender, &shape_layer);
     [CAAnimationRotation animationRoation:shapeLayer];
+}
+
+-(void)recovery:(UIButton *)sender{
+    CAShapeLayer *shapeLayer = objc_getAssociatedObject(sender, &shape_layer);
+    [shapeLayer removeAllAnimations];
 }
 
 
