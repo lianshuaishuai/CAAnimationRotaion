680c071f8115e4dd1300e0f9f84ce8837268475b
连帅帅 <741312975@qq.com>
2021-03-05 10:25:15 +0800

    --提交

--- CAAnimationAnchorRotation/ViewController.m
+++ CAAnimationAnchorRotation/ViewController.m
@@ -58,7 +58,7 @@ static char shape_layer;
 -(void)recovery:(UIButton *)sender{
     UIImageView *shapeLayer = objc_getAssociatedObject(sender, &shape_layer);
     [shapeLayer.layer removeAllAnimations];
-    NSLog(@"4444");
+    NSLog(@"4444555");
 }
 
 @end
